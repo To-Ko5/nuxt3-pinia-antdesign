46d6a6485768a4179b806c7e022f00a04a065772
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-28 21:56:45 +0900

    feat(top): users get add

--- components/Card.vue
+++ components/Card.vue
@@ -1,17 +1,15 @@
 <template>
-  <div>
-    <a-card hoverable style="width: 240px">
-      <template #cover>
-        <img
-          alt="example"
-          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-        />
-      </template>
-      <a-card-meta title="Europe Street beat">
-        <template #description>{{ content }}</template>
-      </a-card-meta>
-    </a-card>
-  </div>
+  <a-card hoverable>
+    <template #cover>
+      <img
+        alt="example"
+        src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+      />
+    </template>
+    <a-card-meta title="Europe Street beat">
+      <template #description>{{ content }}</template>
+    </a-card-meta>
+  </a-card>
 </template>
 
 <script lang="ts" setup>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
   <NuxtLayout name="layout">
-    <Card content="card content" />
+    <a-row :gutter="[16, 16]" :wrap="true" justify="space-between">
+      <a-col v-for="(post, index) in posts" flex="12" class="cards"
+        ><Card :content="post.title" />
+      </a-col>
+    </a-row>
   </NuxtLayout>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+type posts = {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+const { data: posts } = await useFetch<posts[]>(
+  'https://jsonplaceholder.typicode.com/posts'
+)
+</script>
 
 <style lang="scss" scoped></style>
