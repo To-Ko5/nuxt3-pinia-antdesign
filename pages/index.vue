997e64eb612477bfb85de4b9045bf4a90e959c40
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-29 22:36:31 +0900

    feat(post): detail page add

--- pages/index.vue
+++ pages/index.vue
@@ -2,20 +2,15 @@
   <NuxtLayout name="layout">
     <a-row :gutter="[16, 16]" :wrap="true" justify="space-between">
       <a-col v-for="(post, index) in posts" flex="12" class="cards"
-        ><Card :content="post.title" />
+        ><Card :id="post.id" :user-id="post.userId" :content="post.title" />
       </a-col>
     </a-row>
   </NuxtLayout>
 </template>
 
 <script lang="ts" setup>
-type posts = {
-  userId: number
-  id: number
-  title: string
-  body: string
-}
-const { data: posts } = await useFetch<posts[]>(
+import { type post } from '@/types/data'
+const { data: posts } = await useFetch<post[]>(
   'https://jsonplaceholder.typicode.com/posts'
 )
 </script>
---
+++ pages/post/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLayout name="layout">
+    <div>{{ post?.id }}</div>
+    <div>{{ post?.title }}</div>
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+import { type post } from '@/types/data'
+const id = useRoute().params.id
+const { data: post } = await useFetch<post>(
+  `https://jsonplaceholder.typicode.com/posts/${id}`
+)
+</script>
+
+<style lang="scss" scoped></style>
