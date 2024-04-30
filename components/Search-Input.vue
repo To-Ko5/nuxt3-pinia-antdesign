322fbdd7c16c0cdf8c92fae8f57bbe46e6583b20
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-30 18:35:48 +0900

    feat(search): search input component add

---
+++ components/Search-Input.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <a-input-search
+      placeholder="input search text"
+      enter-button="Search"
+      size="large"
+    />
+
+    <!-- <a-input-search
+      v-model:value="value"
+      placeholder="input search text"
+      enter-button="Search"
+      size="large"
+      @search="onSearch"
+    /> -->
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <NuxtLayout name="layout">
+    <div class="search">
+      <search-input />
+    </div>
     <a-row :gutter="[16, 16]" :wrap="true" justify="space-between">
       <a-col v-for="(post, index) in posts" flex="12" class="cards"
         ><Card :id="post.id" :user-id="post.userId" :content="post.title" />
@@ -10,9 +13,17 @@
 
 <script lang="ts" setup>
 import { type post } from '@/types/data'
-const { data: posts } = await useFetch<post[]>(
+const { data } = await useFetch<post[]>(
   'https://jsonplaceholder.typicode.com/posts'
 )
+
+const posts = ref(data)
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.search {
+  margin-bottom: 24px;
+  max-width: 400px;
+  margin-left: auto;
+}
+</style>
